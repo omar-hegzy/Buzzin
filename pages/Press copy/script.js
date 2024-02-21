94c1f9be370a8b3c2f3a29d9c7ff0f287723b206
omar hegazy <omarabdelazizihegazy@gmail.com>
2024-02-21 13:30:44 +0400

    Updated Homepage

--- pages/Press copy/script.js
+++ pages/Press copy/script.js
@@ -10,31 +10,4 @@ toggleBtn.onclick =function() {
   ? 'fa-solid fa-xmark'
   : 'fa-solid fa-bars'
 }
-
-
-let nextBtn =document.getElementById('next');
-let prevBtn =document.getElementById('prev');
-
-
-let lastPosition= list.length -1;
-let active =0;
-
-
-
-
-
-
-nextBtn.onclick =function(){
-
-  const widthItem =document.querySelector('.item').offsetWidth;
-  document.getElementById('cards').scrollLeft += (widthItem);
-
-}
-
-prevBtn.onclick =function(){
-  let newValue= active - 1 < 0 ? lastPosition  : active - 1;
-  const widthItem =document.querySelector('.item').offsetWidth;
-  document.getElementById('cards').scrollLeft -= (widthItem);
-  
-}
-
+//end header
