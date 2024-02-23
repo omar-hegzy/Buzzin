49f9d6d35c64d45c4ee3c0a33a484e1f8add327b
omar hegazy <omarabdelazizihegazy@gmail.com>
2024-02-23 14:14:41 +0400

    New Pages and Updates

--- app.js
+++ app.js
@@ -110,6 +110,37 @@ const hiddenElementsL= document.querySelectorAll('.hidden-l');
 hiddenElementsL.forEach((el) => observerL.observe(el));
 
 // End Features
+//Start Card
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+//End Cards
 
 //Start logos
 var copy = document.querySelector(".slider").cloneNode(true);
